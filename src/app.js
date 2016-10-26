3d9587408ec6968462d5be7bade99e1446a65de1
Yukai Huang <yukaihuangtw@gmail.com>
2016-10-26 17:05:59 +0800

    Config font-awesome

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import 'font-awesome/css/font-awesome.css';
+
 import Vue from 'vue';
 import App from './App.vue';
 
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,12 @@ module.exports = {
 			options: {
 				name: '[name].[ext]?[hash]'
 			}
+		}, {
+			test: /\.css$/,
+			loader: 'style!css?sourceMap'
+		}, {
+			test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+			loader: 'url-loader'
 		}]
 	},
 	resolve: {
@@ -41,7 +47,7 @@ module.exports = {
 
 if (process.env.NODE_ENV === 'production') {
 	module.exports.devtool = '#source-map';
-		// http://vue-loader.vuejs.org/en/workflow/production.html
+	// http://vue-loader.vuejs.org/en/workflow/production.html
 	module.exports.plugins = (module.exports.plugins || []).concat([
 		new webpack.DefinePlugin({
 			'process.env': {
