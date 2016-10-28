5170f60cbcbe0c130f391015577ad15aa680950c
Yukai Huang <yukaihuangtw@gmail.com>
2016-10-28 23:20:29 +0800

    Guard clause for null user

--- src/helpers/userHelper.js
+++ src/helpers/userHelper.js
@@ -1,4 +1,8 @@
 export function avatarURL(user) {
+  if (!user) {
+    return 'http://www.plurk.com/static/default_medium.gif';
+  }
+
   const { has_profile_image, avatar, id } = user;
   if (has_profile_image == 1 && !avatar) {
     return `http://avatars.plurk.com/${id}-medium.gif`;
