d499f4089f62b6cbd83ffe8f56251c9f1a325c86
Yukai Huang <yukaihuangtw@gmail.com>
2016-10-28 12:43:39 +0800

    Update sample API namespace

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,11 @@
 import * as Timeline from './timeline';
+import * as Profile from './profile';
+import * as Responses from './responses';
+import * as Users from './users';
 
 module.exports = {
-  Timeline
+  Timeline,
+  Profile,
+  Responses,
+  Users
 };
