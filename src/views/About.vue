d1d010fde883c54691b0505727970d0bb2dc7120
Yukai Huang <yukaihuangtw@gmail.com>
2016-10-29 09:23:26 +0800

    Display plurk list in about view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,31 +1,55 @@
 <template>
   <div class="about-container">
-    <time-baseline />
     <profile />
+    <time-baseline />
+    <plurk-card v-for="plurk in plurks" :plurk="plurk" :userList="userList" />
   </div>
 </template>
 
 <script>
 import Profile from '../components/Profile.vue';
 import TimeBaseline from '../components/TimeBaseline.vue';
+import PlurkCard from '../components/PlurkCard.vue';
+
+import { getPublicProfile } from '../api/profile';
+import { getPublicPlurks } from '../api/timeline';
 
 export default {
   name: 'About',
 
   components: {
     Profile,
-    TimeBaseline
+    TimeBaseline,
+    PlurkCard
   },
 
   mounted() {
    const { user_id } = this.$route.params;
-   console.log(user_id);
+
+   getPublicProfile(user_id).then(data => {
+     this.profile = data;
+   });
+
+   getPublicPlurks(user_id).then(data => {
+     this.userList = data.plurk_users;
+     this.plurks = data.plurks;
+   });
+  },
+
+  data() {
+    return {
+      userList: null,
+      plurks: [],
+      profile: null
+    };
   }
 }
 </script>
 
 <style lang="sass">
 .about-container {
+  padding: 1em 2em 1em 0.5em;
+
   overflow-y: scroll;
   height: 100%;
 
