cfaadea68f7ffa3a78f744bd909b81dc44da72b8
Yukai Huang <yukaihuangtw@gmail.com>
2016-10-31 08:57:05 +0800

    Add content animation

--- src/views/About.vue
+++ src/views/About.vue
@@ -67,6 +67,11 @@ export default {
 
         let deltaY = e.deltaY;
         const preserveHeight = 48;
+        const maxNameTranslate = 82;
+        const maxContentTranslate = 60;
+
+        let contentMoved;
+        let nameMoved;
 
         // prevent overscrolling
         if (self.scrollLength + deltaY < 0) {
@@ -79,13 +84,27 @@ export default {
 
         // handle virtual scroll value
         if (self.scrollLength < originalHeight) {
+          nameMoved = self.scrollLength / originalHeight * maxNameTranslate;
+          contentMoved = self.scrollLength / originalHeight * maxContentTranslate;
+
           this.scrollTop = 0;
         } else {
+          nameMoved = maxNameTranslate;
+          contentMoved = maxContentTranslate;
           this.scrollTop = self.scrollLength - originalHeight;
         }
 
         let op = self.scrollLength > 0 ? '-' : '+';
         profile.style.height = `calc(14em ${op} ${Math.abs(self.scrollLength)}px)`;
+
+        profile.querySelector('.display-name').style.transform = `translateY(-${nameMoved}px)`;
+
+        profile.querySelector('.avatar').style.transform = `translateY(-${contentMoved}px)`;
+        profile.querySelector('.nickname').style.transform = `translateY(-${contentMoved}px)`;
+
+        const opacityPercentage = contentMoved / maxContentTranslate * 100;
+        profile.querySelector('.avatar img').style.filter = `opacity(${100 - opacityPercentage}%)`;
+        profile.querySelector('.nickname').style.filter = `opacity(${100 - opacityPercentage}%)`;
 
         // console.log(`deltaY: ${deltaY}, scrollLength: ${self.scrollLength}, scrollTop: ${this.scrollTop}, originalHeight: ${originalHeight}`);
       }
