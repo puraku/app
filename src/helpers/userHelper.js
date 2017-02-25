e2e310c2656332fc8568c15742559c6bd2e2c60b
Yukai Huang <yukaihuangtw@gmail.com>
2017-02-25 20:22:02 +0800

    Use https

--- src/helpers/userHelper.js
+++ src/helpers/userHelper.js
@@ -2,15 +2,15 @@ export function avatarURL (user, size = 'medium') {
   const ext = size === 'big' ? 'jpg' : 'gif';
 
   if (!user) {
-    return `http://www.plurk.com/static/default_${size}.${ext}`;
+    return `https://www.plurk.com/static/default_${size}.${ext}`;
   }
 
   const { has_profile_image, avatar, id } = user;
   if (has_profile_image === 1 && !avatar) {
-    return `http://avatars.plurk.com/${id}-${size}.${ext}`;
+    return `https://avatars.plurk.com/${id}-${size}.${ext}`;
   } else if (has_profile_image === 1 && avatar) {
-    return `http://avatars.plurk.com/${id}-${size}${avatar}.${ext}`;
+    return `https://avatars.plurk.com/${id}-${size}${avatar}.${ext}`;
   } else {
-    return `http://www.plurk.com/static/default_${size}.${ext}`;
+    return `https://www.plurk.com/static/default_${size}.${ext}`;
   }
 }
